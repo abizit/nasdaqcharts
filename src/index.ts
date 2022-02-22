ac0c35f2e1843141f7aff17f2344116e2fe80a50
Abhijeet Bajracharya <abhijeetbajracharya@gmail.com>
2022-02-22 22:24:53 +0545

    NASDAQCHARTS main class
    - nasdaq service injected
    - form submission handled

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 import "./styles.css";
 import {NasdaqService} from "./modules/index";
 
-const nasdaqClient = new NasdaqService()
 
-const app = async () => {
-    const data = await nasdaqClient._fetchAPI('AAPL')
-    console.log(data)
+class NasDaqCharts {
+    nasdaqService: NasdaqService;
+    inputBox: HTMLFormElement;
+
+    constructor() {
+        this.nasdaqService = new NasdaqService();
+        this.inputBox = document.getElementById('scrip') as HTMLFormElement;
+    }
+
+    async searchScrip() {
+        const data = await this.getData(this.inputBox.value)
+        console.log(data)
+    }
+
+    async getData(scrip: string) {
+        return await this.nasdaqService._fetchAPI(scrip)
+    }
+
 }
-app().then(
-    () => console.log('APP LOADED')
-)
+
+const nasdaqcharts = new NasDaqCharts()
+const form = document.getElementById('scripSearch');
+const searchSubmit = (event: Event) => {
+    event.preventDefault();
+    nasdaqcharts.searchScrip().then()
+}
+form.addEventListener('submit', searchSubmit);
+
--- src/modules/nasdaq.service.ts
+++ src/modules/nasdaq.service.ts
@@ -3,14 +3,14 @@ interface Options {
 }
 
 export class NasdaqService {
-    private _baseURL: string = 'https://data.nasdaq.com/api/v3/datasets/WIKI/';
+    private _baseURL: string = "https://data.nasdaq.com/api/v3/datasets/WIKI/";
 
     constructor() {
     }
 
     async _fetchAPI(endpoint: string) {
-        const res = await fetch(this._baseURL + endpoint);
-
+        const res = await fetch(this._baseURL + endpoint + '/data.json?api_key=-vvbvunv-Nvs3zq8PctX');
+        // const res = await fetch('https://data.nasdaq.com/api/v3/datasets/WIKI/APPL.json')
         if (!res.ok) throw new Error(res.statusText);
 
         if (res.status !== 204) return res.json();
