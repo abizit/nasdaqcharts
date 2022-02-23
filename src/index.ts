cdd780e4fce44744fccdef9fcac4147e1c8e155e
Abhijeet Bajracharya <abhijeetbajracharya@gmail.com>
2022-02-23 13:25:56 +0545

    Chart.js added

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import "./styles.css";
 import {NasdaqService} from "./modules/index";
+import {Chart, registerables} from "chart.js";
+
+Chart.register(...registerables);
 
 
 class NasDaqCharts {
     nasdaqService: NasdaqService;
     inputBox: HTMLFormElement;
+    canvas: HTMLCanvasElement;
+    context: CanvasRenderingContext2D;
 
     constructor() {
         this.nasdaqService = new NasdaqService();
         this.inputBox = document.getElementById('scrip') as HTMLFormElement;
+        this.canvas = document.getElementById('myChart') as HTMLCanvasElement;
+        this.context = this.canvas.getContext('2d');
     }
 
     async searchScrip() {
