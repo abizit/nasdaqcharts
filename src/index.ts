7b4ed01376bf3800c2d61bd4087ed11512ab215b
Abhijeet Bajracharya <abhijeetbajracharya@gmail.com>
2022-02-23 17:41:33 +0545

    Render Charts
    Format the api data to Chart.js compatible data and render the close price chart

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "./styles.css";
 import {NasdaqService} from "./modules/index";
 import {Chart, registerables} from "chart.js";
+import {ChartData, Dataset, DatasetData} from "./interfaces/interfaces";
 
 Chart.register(...registerables);
 
@@ -10,21 +11,62 @@ class NasDaqCharts {
     inputBox: HTMLFormElement;
     canvas: HTMLCanvasElement;
     context: CanvasRenderingContext2D;
+    dataset: DatasetData
+    chartData: ChartData[] = [];
+    scripChart: Chart;
 
     constructor() {
         this.nasdaqService = new NasdaqService();
         this.inputBox = document.getElementById('scrip') as HTMLFormElement;
         this.canvas = document.getElementById('myChart') as HTMLCanvasElement;
         this.context = this.canvas.getContext('2d');
+
     }
 
     async searchScrip() {
         const data = await this.getData(this.inputBox.value)
-        console.log(data)
+        this.dataset = data.dataset_data;
+        this.formatData()
     }
 
-    async getData(scrip: string) {
+    async getData(scrip: string): Promise<Dataset> {
         return await this.nasdaqService._fetchAPI(scrip)
+    }
+
+    private formatData() {
+        for (const data of this.dataset.data) {
+            let dayData: ChartData = {
+                date: data[0],
+                open: data[1],
+                high: data[2],
+                low: data[3],
+                close: data[4]
+            }
+            this.chartData = [...this.chartData, dayData]
+        }
+        this.renderChart();
+    }
+
+    private renderChart() {
+        let columns: string[] = [],
+            daydata: number[] = [];
+        for (const day of this.chartData) {
+            columns = [...columns, day.date];
+            daydata = [...daydata, day.close]
+        }
+        if (this.scripChart) {
+            this.scripChart.destroy()
+        }
+        this.scripChart = new Chart(this.context, {
+            type: 'line',
+            data: {
+                labels: columns,
+                datasets: [{
+                    label: 'Close Price $',
+                    data: daydata
+                }]
+            }
+        })
     }
 
 }
