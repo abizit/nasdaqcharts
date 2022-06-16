c56ae811311fe5c9552501c6e0cafb7763e82c6b
Abhijeet Bajracharya <abhijeetbajracharya@gmail.com>
2022-06-16 10:17:50 +0545

    Code Cleanup

--- src/modules/nasdaq.service.ts
+++ src/modules/nasdaq.service.ts
@@ -10,7 +10,6 @@ export class NasdaqService {
 
     async _fetchAPI(endpoint: string) {
         const res = await fetch(this._baseURL + endpoint + '/data.json?api_key=-vvbvunv-Nvs3zq8PctX');
-        // const res = await fetch('https://data.nasdaq.com/api/v3/datasets/WIKI/APPL.json')
         if (!res.ok) throw new Error(res.statusText);
 
         if (res.status !== 204) return res.json();
